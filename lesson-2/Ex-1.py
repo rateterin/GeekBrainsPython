fe3688de8404c3556fcac82c40b540841b807816
Роман Тетерин <rateterin@yandex.ru>
2020-10-05 11:35:32 +0300

    Exsercises 1 - 6 for lesson 2

---
+++ lesson-2/Ex-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+#
+# Written by Teterin Roman
+# for GeekBrains Python course
+# Python base
+# Lesson 2 Exercise 1
+
+lst = [1, -4.5, 'String', [1, 2, 'g'], (1, 2, 'g'), {1, 2, 'g'}, {'name': 'Ira', 'gender': 'female', 'age': 18}]
+for list_item in lst:
+    print(type(list_item), ' ', list_item)
---
+++ lesson-2/Ex-5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python
+#
+# Written by Teterin Roman
+# for GeekBrains Python course
+# Python base
+# Lesson 2 Exercise 2
+
+my_list = [7, 5, 3, 3, 2]
+r = int(input('Введите рейтинг (целое число): '))
+my_list.append(r)
+my_list.sort(reverse=True)
+print(my_list)
